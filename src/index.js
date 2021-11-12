e785f50d612c26899877377b88ec566bd849ec65
Kadeem140 <Beckford.kadeem@yahoo.com>
2021-11-11 22:54:20 -0500

    MVP completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const inputField = document.querySelector("#new-task-description");
+const addBtn = document.querySelector("button");
+const todoList = document.querySelector("#tasks");
+
+//Select the Button
+//Add a Click Event to this button
+//Prevent the Default 
+addBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  let userData = inputField.value;
+  let getlocalStorage = localStorage.getItem("tasks"); //getting localStorage
+
+  if(getlocalStorage == null){
+    listArr = []; //create empty array
+  }else{
+    listArr = JSON.parse(getlocalStorage);
+    console.log(listArr)
+  }
+  listArr.push(inputField.value);
+  localStorage.setItem("tasks", JSON.stringify(listArr)); //transform js object
+  ShowTasks(); //Calls function
+});
+
+function ShowTasks(){
+  let getlocalStorage = localStorage.getItem("tasks"); //getting localStorage
+  if(getlocalStorage == null){
+    listArr = []; //create empty array
+  } else{
+    listArr = JSON.parse(getlocalStorage);
+    console.log(listArr)
+  }
+  let newLiTag = "";
+  listArr.forEach((element, index) => {
+    newLiTag = `<li> ${element}</li>`
+  });
+  todoList.innerHTML = newLiTag // adding new tasks
+}
